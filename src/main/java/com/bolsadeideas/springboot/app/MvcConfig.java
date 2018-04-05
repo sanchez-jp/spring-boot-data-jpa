b739fb727290dd931226f971fb589c4ccc536cf7
Juan Pedro Sánchez Álvarez <sanchezalvarezjp@gmail.com>
2018-04-05 11:38:06 +0200

    Implementación del método passwordEncoder.
      Implementación del método passwordEncoder en la clase MvcConfig para
      la codificación de contraseñas haciendo uso del codificador BCrypt
      Password Encoder, recomendado por Spring Security y uno de los más
      potentes en cuanto a seguridad actualmente.

--- src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
+++ src/main/java/com/bolsadeideas/springboot/app/MvcConfig.java
@@ -2,7 +2,9 @@ package com.bolsadeideas.springboot.app;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -41,6 +43,11 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
 
         registry.addViewController("/error_403").setViewName("error_403");
+    }
+
+    @Bean // Indica que es un componente Bean de Spring
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 
